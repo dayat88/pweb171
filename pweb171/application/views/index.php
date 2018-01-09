51b565babb0c10b85b664fa411300cff46d60bae
dayat88 <32377546+dayat88@users.noreply.github.com>
2018-01-09 23:55:31 +0800

    pweb171
    tgs

---
+++ pweb171/application/views/index.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Lagu Anak</title>
+    <link rel="stylesheet" href="/css/style.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <h1 id="judul">Koleksi lagu anak</h1>
+    <hr>
+<div class="">
+
+    <div id="menu">
+
+      <a href="<?php echo site_url('welcome/menu1') ?>">Pelangi</a><br>
+      <a href="<?php echo site_url('welcome/menu2') ?>">Balon Ku</a><br>
+      <a href="<?php echo site_url('welcome/menu3') ?>">Kereta</a><br>
+      <a href="<?php echo site_url('welcome/menu4') ?>">Cicak</a><br>
+      <a href="<?php echo site_url('welcome/menu5') ?>">Kebun Ku</a><br>
+
+    </div>
+
+</div>
+    <br>
+    <br>
+    <div id="copyright">
+          Didesain oleh: Alfredo Hidayah.T &copy; 2017
+        </div>
+    </div>
+
+
+
+  </body>
+</html>
---
+++ pweb171/application/views/pelangi.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Lagu Anak</title>
+    <link rel="stylesheet" href="/css/style.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <h1 id="judul">Koleksi Lagu Anak</h1>
+    <hr>
+
+    <h1 id="jl">Pelangi</h1>
+    <img src="/img/Pelangi.jpg">
+
+    <p id="lagu">
+      Pelangi-pelangi<br>
+      Alangkah indahmu<br>
+      Merah-kuning-hijau<br>
+      Di langit yang biru<br>
+      Pelukismu Agung<br>
+      Siapa gerangan<br>
+      Pelangi-pelangi<br>
+      Ciptaan Tuhan<br>
+    </p>
+<a href="<?php echo site_url('welcome/home') ?>">Home</a>
+
+  </body>
+</html>
